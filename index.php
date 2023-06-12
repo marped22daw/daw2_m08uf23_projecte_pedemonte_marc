08f478e08bdcdfdad9ce59a2566ba62188482fc2
marped22daw <113631662+marped22daw@users.noreply.github.com>
2023-06-12 17:19:25 +0200

    Primer Commit Projecte m08uf23

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>
+			PÀGINA WEB INICIAL DE L'APLICACIÓ
+		</title>
+	</head>
+	<body>
+		<h1> APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h1>
+		<h2> DAW2 M08UF2 M08UF3 </h2>
+		<h3> Autor: marped22daw</h3>
+		<h3> Correu: marped22daw2@protonmail.com</h3>
+		<h3> Github: https://github.com/marped22daw/autent.git</h3>
+		<a href="http://zend-mapema.fjeclot.net/autent/login.php">Inicia sessió</a>
+	</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>
+			PÀGINA WEB DEL MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP
+		</title>
+	</head>
+	<body>
+		<h2> MENÚ PRINCIPAL DE L'APLICACIÓ D'ACCÉS A BASES DE DADES LDAP</h2>
+		<a href="http://zend-mapema.fjeclot.net/autent/crearusuari.php">Crear usuari</a><br>
+		<a href="http://zend-mapema.fjeclot.net/autent/visualitzarusuari.php">Visualitzar usuari</a><br>
+		<a href="http://zend-mapema.fjeclot.net/autent/modificarusuari.php">Modificar usuari</a><br>
+		<a href="http://zend-mapema.fjeclot.net/autent/eliminarusuari.php">Eliminar usuari</a><br>
+		<a href="http://zend-mapema.fjeclot.net/autent/index.php">Torna a la pàgina inicial</a><br>
+	</body>
+</html>
